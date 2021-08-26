e8c3b98eb8a475ece3ddb48b6f17b53039b465fc
Soheil Solhjoo <46877039+soheilsolhjoo@users.noreply.github.com>
2021-08-26 17:05:01 +0200

    Update HD_exponential.m
    Updated disclaimer

--- private/HD_exponential.m
+++ private/HD_exponential.m
@@ -43,6 +43,7 @@
 %   HDFT  Copyright (C) 2021  Soheil Solhjoo
 %   This program comes with ABSOLUTELY NO WARRANTY.
 %   This is free software, and you are welcome to redistribute it under certain conditions.
+%   Check "copyright.txt" in the main folder.
 %
 %           Soheil Solhjoo
 %           February 17, 2021
