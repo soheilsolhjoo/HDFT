ab31309c22a0128a27f1e5b4c5a12962b7eca561
Soheil Solhjoo <46877039+soheilsolhjoo@users.noreply.github.com>
2021-08-26 17:05:14 +0200

    Update HD_g_mesher.m
    Updated disclaimer

--- private/HD_g_mesher.m
+++ private/HD_g_mesher.m
@@ -4,7 +4,10 @@
 % Zenner-Holomon parameter.
 % 
 %% DISCLAIMER
-% This program is provided as is for free use. 
+%   HDFT  Copyright (C) 2021  Soheil Solhjoo
+%   This program comes with ABSOLUTELY NO WARRANTY.
+%   This is free software, and you are welcome to redistribute it under certain conditions.
+%   Check "copyright.txt" in the main folder.
 % 
 %           Soheil Solhjoo
 %           February 17, 2021
