ada7d2ca802b973df10806e1b26c9429fd50a99d
Soheil Solhjoo <46877039+soheilsolhjoo@users.noreply.github.com>
2021-09-16 16:44:58 +0200

    Update HD_runner.m

--- HD_runner.m
+++ HD_runner.m
@@ -4,7 +4,7 @@ clear; close all;
 CK = 1;
 N = 100;
 picked_strain = 0.05;
-const = 0;
+const = 1;
 poly_type = 'poly9';
 revised_ST_method = 1;
 Q_normliazer = 1000;
@@ -14,11 +14,11 @@ ANN_struct = [2,5];
 ANN_alg = 'trainbr';
 
 % functions call
-HD_power_law(CK,N,picked_strain,const,poly_type,save_flag);
-HD_exponential(CK,N,picked_strain,const,poly_type,save_flag);
-HD_sinh_conventional(CK,N,picked_strain,[const,const],poly_type,save_flag);
-HD_sinh_revisited(CK,N,picked_strain,[const,const],poly_type,revised_ST_method,Q_normliazer,save_flag,model_gen);
-HD_ANN(CK,ANN_struct,ANN_alg,save_flag,model_gen);
+HD_f_power_law(CK,N,picked_strain,const,poly_type,save_flag);
+HD_f_exponential(CK,N,picked_strain,const,poly_type,save_flag);
+HD_f_sinh_conventional(CK,N,picked_strain,[const,const],poly_type,save_flag);
+HD_f_sinh_revisited(CK,N,picked_strain,[const,const],poly_type,revised_ST_method,Q_normliazer,save_flag,model_gen);
+HD_f_ANN(CK,N,ANN_struct,ANN_alg,save_flag,model_gen);
 
 % produce plots
 HD_g_full_fig
